75dd41a9713830180d45f34a37367b80462cb1eb
spacha1986 <142676465+spacha1986@users.noreply.github.com>
2025-07-04 16:38:34 +0530

    Update AutomationIds2.kt

--- design/src/main/java/com/medtronic/design/android/util/AutomationIds2.kt
+++ design/src/main/java/com/medtronic/design/android/util/AutomationIds2.kt
@@ -1,4 +1,13 @@
 package com.medtronic.design.android.util
 
 object AutomationIDs2 {
+  object Common {
+        const val LABEL = "label"
+        const val ICON = "icon"
+        const val TITLE = "title"
+        const val PLACEHOLDER = "placeholder"
+        const val POPOVER = "popover"
+        const val OPTION = "option_"
+        const val INDICATOR = "indicator"
+    }
 }
